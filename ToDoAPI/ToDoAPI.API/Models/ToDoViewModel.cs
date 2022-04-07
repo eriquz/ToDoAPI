445b9b8235db7a6b169f4fe21bfa60a7f4060d90
Enrique Garcia <EriquzGar10@outlook.com>
2022-04-07 05:39:30 -0500

    Established API and API DATA.EF projects, Creating the ViewModel and controllers and functios.

---
+++ ToDoAPI/ToDoAPI.API/Models/ToDoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace ToDoAPI.API.Models
+{
+    public class ToDoViewModel
+    {
+        public int TodoId { get; set; }
+        public string Action { get; set; }
+        public bool Done { get; set; }
+        public int CategoryID { get; set; }
+
+        public virtual CategoryViewModel Category { get; set; }
+    }
+
+    public class CategoryViewModel
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
